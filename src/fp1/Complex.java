88ef2bd6f5f18577b4d26dbdd3a6f0ed1b214369
Pete Cappello <cappello@cs.ucsv.edu>
2016-04-02 11:36:07 -0700

    Put code in Complex class.

--- src/fp1/Complex.java
+++ src/fp1/Complex.java
@@ -1,5 +1,8 @@
 package fp1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Peter Cappello
@@ -14,11 +17,56 @@ public class Complex {
         }
         
         Complex add( Complex operand ) {
-            return new Complex( real + operand.real, imag + operand.imag );
+            return new Complex( real + operand.real,  imag + operand.imag );
         }
         
         Complex multiply( Complex operand ) {
             return new Complex( real * operand.real - imag * operand.imag, 
                                 imag * operand.real + real * operand.imag );
         }
+        
+        double size() { return Math.sqrt( real * real + imag * imag ); }
+        
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append( " (").append( real ).append( ',' ).append( imag ).append( ") ");
+            return new String( stringBuilder );
+        }
+        
+        public static void main( String[] args ) {
+        List<Complex> complexList = new ArrayList<>();
+        complexList.add( new Complex( 6, 3 ) );
+        complexList.add( new Complex( 2, 2 ) );
+        complexList.add( new Complex( 5, 8 ) );
+        complexList.add( new Complex( 1, 5 ) );
+        complexList.add( new Complex( 1, 6 ) );
+        complexList.add( new Complex( 2, 7 ) );
+        complexList.add( new Complex( 2, 8 ) );
+        complexList.add( new Complex( 6, 5 ) );
+        complexList.add( new Complex( 1, 3 ) );
+        complexList.add( new Complex( 6, 6 ) );
+        System.out.println( complexList );
+        
+        // Sum the sizes of the magnified and translated complex numbers whose original size is < 3.0.
+        // imperatively
+        double sum = 0.0;
+        Complex translate = new Complex( 1, 1 );
+        Complex magnify = new Complex( 2, 2 );
+        for ( Complex num : complexList )
+        {
+            if ( num.size() < 3.0 ) {
+                sum += num.multiply( magnify ).add( translate ).size();
+            }
+        }
+        System.out.println( "sum: " + sum );
+        
+        // declaratively
+        final double declarativeSum = 
+              complexList.stream()
+                         .filter( num -> num.size() < 3 )
+                         .mapToDouble( num -> num.multiply( new Complex(2, 2) ).add( new Complex(1, 1) ).size() )
+                         .sum();
+        System.out.println( "declarativeSum: " + declarativeSum );                
+    }
 }
